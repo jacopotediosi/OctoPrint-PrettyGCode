b561b321939e4b6750797a4c677c7b999da4b3f6
Jacopo Tediosi <jacopotediosi@gmail.com>
2026-07-29 13:40:52 +0200

    Bump pre-commit deps

--- octoprint_prettygcode/__init__.py
+++ octoprint_prettygcode/__init__.py
@@ -10,32 +10,32 @@ class PrettyGCodePlugin(
     octoprint.plugin.TemplatePlugin,
 ):
     def get_assets(self):
-        return dict(
-            js=[
+        return {
+            "js": [
                 "js/pg-main.bundle.js",  # app bundle, built by `task build-frontend`
                 "js/prettygcode.js",
             ],
-            css=["css/prettygcode.css"],
-        )
+            "css": ["css/prettygcode.css"],
+        }
 
     def is_template_autoescaped(self):
         return True
 
     def get_template_vars(self):
-        return dict(github_url=GITHUB_URL)
+        return {"github_url": GITHUB_URL}
 
     def get_update_information(self):
-        return dict(
-            prettygcode=dict(
-                displayName=self._plugin_name,
-                displayVersion=self._plugin_version,
-                type="github_release",
-                user="jacopotediosi",
-                repo="OctoPrint-PrettyGCode",
-                current=self._plugin_version,
-                pip=GITHUB_URL + "/archive/{target_version}.zip",
-            )
-        )
+        return {
+            "prettygcode": {
+                "displayName": self._plugin_name,
+                "displayVersion": self._plugin_version,
+                "type": "github_release",
+                "user": "jacopotediosi",
+                "repo": "OctoPrint-PrettyGCode",
+                "current": self._plugin_version,
+                "pip": GITHUB_URL + "/archive/{target_version}.zip",
+            }
+        }
 
 
 __plugin_name__ = "PrettyGCode"
